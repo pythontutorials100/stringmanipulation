510ad21f835cbec4ebb2a4a27a8399f43273757d
pythontutorials100 <systems.arw@gmail.com>
2024-11-20 09:52:09 -0800

    Delete strintval directory

--- strintval/allgraph.py
+++
@@ -1,7 +0,0 @@
-CONSTRUCT {
-  ?subject ?predicate ?object .
-}
-WHERE {
-  ?subject ?predicate ?object .
-  FILTER (?subject != owl:Class && ?object != owl:Class && ?subject != owl:Thing && ?object != owl:Thing)
-}
